75be5340e9e478c2dc681a8d8bee57a58c09b5e3
KangJiSseok <103586440+KangJiSseok@users.noreply.github.com>
2024-02-26 15:32:12 +0900

    [Silver IV] Title: 카드2, Time: 196 ms, Memory: 45472 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 1; i <= N; i++) {
+            queue.add(i);
+        }
+
+        while (queue.size() != 1) {
+            Integer remove = queue.remove();
+            Integer poll = queue.poll();
+            queue.add(poll);
+        }
+
+        System.out.println(queue.poll());
+    }
+}
