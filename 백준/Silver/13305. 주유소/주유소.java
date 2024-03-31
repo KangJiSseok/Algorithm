a1ef31902829ad9e83db00ab4989e1a7a3030375
KangJiSseok <103586440+KangJiSseok@users.noreply.github.com>
2024-03-31 17:58:04 +0900

    [Silver III] Title: 주유소, Time: 360 ms, Memory: 36044 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -9,35 +9,30 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int gasCost = 0;
+        long gasCost = 0;
 
         //도시 개수
         int nValue = Integer.parseInt(br.readLine());
 
-        int[] cityConnectLine = new int[nValue-1];
-        int[] cityGas = new int[nValue];
+        long[] cityConnectLine = new long[nValue - 1];
+        long[] cityGas = new long[nValue];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
         for (int i = 0; i < nValue - 1; i++) {
             cityConnectLine[i] = Integer.parseInt(st.nextToken());
-        }
-
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < nValue; i++) {
-            cityGas[i] = Integer.parseInt(st.nextToken());
+            cityGas[i] = Integer.parseInt(st2.nextToken());
         }
 
         for (int i = 0; i < cityGas.length - 1; i++) {
             if (cityGas[i] < cityGas[i + 1]) {
                 cityGas[i + 1] = cityGas[i];
             }
-        }
 
-        for (int i = 0; i < cityGas.length - 1; i++) {
             gasCost += cityGas[i] * cityConnectLine[i];
         }
 
         System.out.println(gasCost);
+        br.close();
     }
 }
