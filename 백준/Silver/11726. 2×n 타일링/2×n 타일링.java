e17d42349ef0a5239f687da90b24ffb8af31ca87
KangJiSseok <103586440+KangJiSseok@users.noreply.github.com>
2024-04-10 23:35:26 +0900

    [Silver III] Title: 2×n 타일링, Time: 128 ms, Memory: 14404 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int[] Rectangle = new int[1001];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int result = dp(N);
+        System.out.println(result);
+    }
+
+    static private int dp(int n) {
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+        if(Rectangle[n] != 0) return Rectangle[n];
+        return Rectangle[n] = (dp(n - 1) + dp(n - 2)) % 10007;
+    }
+}
