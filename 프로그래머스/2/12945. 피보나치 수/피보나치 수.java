001911a65b8a7c296f29f39d18076c5e3eb62b14
KangJiSseok <103586440+KangJiSseok@users.noreply.github.com>
2024-06-08 18:00:45 +0900

    [level 2] Title: 피보나치 수, Time: 19.40 ms, Memory: 86.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,17 @@
+class Solution {
+    
+    static int[] arr = new int[100001];
+    
+    public int solution(int n) {
+        
+        int answer = fivo(n);
+        return answer;
+    }
+    
+    public int fivo(int n){
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        if(arr[n] != 0 ) return arr[n];
+        return arr[n] = (fivo(n-1) + fivo(n-2)) % 1234567;
+    }
+}
