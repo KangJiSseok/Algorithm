577a8d6d3c1e49c40ca76162d759a025a273755a
KangJiSseok <103586440+KangJiSseok@users.noreply.github.com>
2024-06-10 11:06:27 +0900

    [level 2] Title: 요격 시스템, Time: 0.48 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] targets) {
+        
+        
+        Arrays.sort(targets, (o1, o2) -> 
+                    {
+                        return o1[1] - o2[1];
+                    });
+        
+        // for(int i=0; i< targets.length; i++){
+        //     for(int j=0; j<targets[0].length; j++){
+        //         System.out.print(targets[i][j]+ " ");
+        //     }
+        //     System.out.println();
+        // }
+        
+        // 1 끝점 < 2시작점
+        
+        //start
+        int tail = targets[0][0];
+        int head = targets[0][1];
+        int result = 1;
+        
+        for(int i=0; i< targets.length; i++){
+            int currentTail = targets[i][0];
+            int currentHead = targets[i][1];
+            
+            if(head <= currentTail){
+                tail = currentTail;
+                head = currentHead;
+                result++;
+                // System.out.print(i + " ");
+            }
+            
+        }
+        
+        return result;
+    }
+}
