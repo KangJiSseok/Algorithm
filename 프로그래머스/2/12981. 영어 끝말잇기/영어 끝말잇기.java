48c2519944bd39c317721fa43e5fb4620bcf44d3
KangJiSseok <103586440+KangJiSseok@users.noreply.github.com>
2024-08-04 18:11:00 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.11 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -4,23 +4,28 @@ import java.io.*;
 class Solution {
     public int[] solution(int n, String[] words) {
         StringBuilder sb = new StringBuilder();
-        int[] answer = new int[2];                
+        int[] answer = new int[2];       
+        
+        Map<String,Integer> map = new HashMap<>();
+        
         answer[0] = 0;
         answer[1] = 0;
         
         ArrayList<String> list = new ArrayList<>();
         
         list.add(words[0]);
+        map.put(words[0],1);
         
         int index = 0;
         for(int i=1; i< words.length; i++){
-            if(list.get(index).charAt(list.get(index).length()-1) != words[i].charAt(0) 
-                || list.contains(words[i])
+            if(list.get(index).charAt(list.get(index).length()-1) != words[i].charAt(0) ||
+              map.get(words[i]) != null
               ){
                 answer[0] = (i%n) + 1;
                 answer[1] = (i/n) + 1;
                 break;
             }
+            map.put(words[i],1);
             list.add(words[i]);
             index++;
         }
