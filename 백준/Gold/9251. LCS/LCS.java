a3da426a0a33c0f3f09a2c1fe0cb1b74716b8d66
KangJiSseok <103586440+KangJiSseok@users.noreply.github.com>
2024-08-26 03:58:03 +0900

    [Gold V] Title: LCS, Time: 100 ms, Memory: 12092 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    static int[][] dp = new int[1001][1001];
+    static int[][] dp = new int[2][1001];
 
     public static void main(String[] args) throws IOException {
 
@@ -13,17 +13,20 @@ public class Main {
         String text = br.readLine();
         String compareText = br.readLine();
 
+        int index = 0;
+
         for (int i = 1; i <= text.length(); i++) {
             for (int j = 1; j <= compareText.length(); j++) {
                 if (text.charAt(i - 1) == compareText.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    dp[index][j] = dp[index^1][j - 1] + 1;
                 } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                    dp[index][j] = Math.max(dp[index^1][j], dp[index][j - 1]);
                 }
             }
+            index ^= 1;
         }
 
-        System.out.println(dp[text.length()][compareText.length()]);
+        System.out.println(dp[index^1][compareText.length()]);
 
     }
 }
