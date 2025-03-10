7b643ce2cacd6ddf9e8c253505403dc3d4c579e2
KANG JISEOK <rkdwltjr5876@naver.com>
2025-03-10 18:48:01 +0900

    250310

---
+++ LeetCode/L3208.java
@@ -0,0 +1,40 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class L3208 {
+	public int numberOfAlternatingGroups(int[] colors, int k) {
+
+		int result = 0;
+		Deque<Integer> d = new LinkedList<>();
+
+		int[] copy = new int[colors.length - 1 + k];
+
+		for(int i= 0 ; i< colors.length; i++){
+			copy[i] = colors[i];
+		}
+
+		for(int i = colors.length; i < copy.length; i++){
+			copy[i] = colors[i - colors.length];
+		}
+
+		int l = 0;
+		int r = 0;
+
+		while(r + 1< copy.length ){
+			if(copy[r] != copy[r + 1]){
+				r++;
+			}else{
+				r = r + 1;
+				l = r;
+			}
+
+			if(r - l + 1== k) {
+				result++;
+				l++;
+			}
+
+		}
+
+		return result;
+	}
+}
