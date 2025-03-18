efe9e4cca13617b337a4265801b93a276c47872c
KANG JISEOK <rkdwltjr5876@naver.com>
2025-03-18 11:32:52 +0900

    250317

---
+++ LeetCode/L2206.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+
+public class L2206 {
+	public boolean divideArray(int[] nums) {
+		HashMap<Integer,Integer> map = new HashMap<>();
+		for(int i=0; i< nums.length; i++){
+			map.put(nums[i],map.getOrDefault(nums[i],0) + 1);
+		}
+
+		for(Integer key : map.keySet()){
+			if(map.get(key)%2 != 0) return false;
+		}
+
+		return true;
+	}
+}
